ad0d6e426a7f08799b84aba2bc048f69a68c9699
bpgualotuna <bpgualotuna@espe.edu.ec>
2025-05-14 11:48:04 -0500

    Funciones de cuentas.js

--- cuentas.js
+++ cuentas.js
@@ -16,6 +16,23 @@ mostrarCuentas=function(){
         Columnas: NUMERO CUENTA, NOMBRE, SALDO
         En la columna NOMBRE concatenar el nombre y el apellido
     */
+    let cmpTabla=document.getElementById("inferior");
+    let contenidoTabla= "<table><tr>" +
+    "<th>NUMERO CUENTA</th>" +
+    "<th>NOMBRE</th>" +
+    "<th>SALDO</th>" +
+    "</tr>"
+    let elementoCuentas;
+    for(let i=0;i<cuentas.length;i++){
+        elementoCuentas=cuentas[i];
+        contenidoTabla+="<tr>" +
+        "<td>"+elementoCuentas.numeroCuenta+"</td>" +
+        "<td>"+elementoCuentas.nombre+" "+elementoCuentas.apellido+"</td>" +
+        "<td>"+elementoCuentas.saldo+"</td>" +
+        "</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
 
 /*
@@ -23,7 +40,14 @@ mostrarCuentas=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
-
+    let cuentaEncontrada=null;
+    for(let i=0;i<cuentas.length;i++){
+        if(cuentas[i].numeroCuenta==numeroCuenta){
+            cuentaEncontrada=cuentas[i];
+            break;
+        }
+    }
+    return cuentaEncontrada;
 }
 
 /*
@@ -33,6 +57,13 @@ buscarCuenta=function(numeroCuenta){
 agregarCuenta=function(cuenta){
     //Si ya existe mostrar un alert CUENTA EXISTENTE
     //Si se agrega, mostrar un alert CUENTA AGREGADA
+    cuentaExiste = buscarCuenta(cuenta.numeroCuenta);
+    if(cuentaExiste!=null){
+        alert("CUENTA EXISTENTE");
+    }else{
+        cuentas.push(cuenta);
+        alert("CUENTA AGREGADA");
+    }
 }
 
 agregar=function(){
@@ -40,4 +71,17 @@ agregar=function(){
     //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
     //Invoca a agregarCuenta
     //Invoca a mostrarCuentas
+    let cedula = recuperarTexto("txtCedula");
+    let numeroCuenta = recuperarTexto("txtNumeroCuenta");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let saldo = parseFloat(recuperarTexto("txtSaldo"));
+    let cuenta = [];
+    cuenta.numeroCuenta = numeroCuenta;
+    cuenta.cedula = cedula;
+    cuenta.nombre = nombre;
+    cuenta.apellido = apellido;
+    cuenta.saldo = saldo;
+    agregarCuenta(cuenta);
+    mostrarCuentas();
 }
