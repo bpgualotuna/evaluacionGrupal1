dc51ea537ab3e0b2df3d236663e5eaf609f7e116
bpgualotuna <bpgualotuna@espe.edu.ec>
2025-05-14 20:52:44 -0500

    implementacion de integracion.js

--- integracion.js
+++ integracion.js
@@ -47,4 +47,230 @@ cargarMovimientos = function () {
 //con el tipo D, que corresponde a DEBITO, el número de cuenta a la que se hizo el retiro
 //y el monto que se retiró. Este objeto movimiento se agrega al arreglo movimientos
 
+// Cuentas.js
+mostrarCuentas=function(){
+    /*
+        Muestra en pantalla una tabla con la información de todas las cuentas del arreglo.
+        Columnas: NUMERO CUENTA, NOMBRE, SALDO
+        En la columna NOMBRE concatenar el nombre y el apellido
+    */
+    let cmpTabla=document.getElementById("inferior");
+    let contenidoTabla= "<table><tr>" +
+    "<th>NUMERO CUENTA</th>" +
+    "<th>NOMBRE</th>" +
+    "<th>SALDO</th>" +
+    "</tr>"
+    let elementoCuentas;
+    for(let i=0;i<cuentas.length;i++){
+        elementoCuentas=cuentas[i];
+        contenidoTabla+="<tr>" +
+        "<td>"+elementoCuentas.numeroCuenta+"</td>" +
+        "<td>"+elementoCuentas.nombre+" "+elementoCuentas.apellido+"</td>" +
+        "<td>"+elementoCuentas.saldo+"</td>" +
+        "</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
 
+/*
+    Busca la cuenta en el arreglo en función del número de cuenta,
+    si existe retorna el objeto cuenta, caso contrario retorna null. 
+*/
+buscarCuenta=function(numeroCuenta){
+    let cuentaEncontrada=null;
+    for(let i=0;i<cuentas.length;i++){
+        if(cuentas[i].numeroCuenta==numeroCuenta){
+            cuentaEncontrada=cuentas[i];
+            break;
+        }
+    }
+    return cuentaEncontrada;
+}
+
+/*
+    Agrega una cuenta al arreglo, solamente si no existe otra cuenta con el mismo numero.
+    No retorna nada
+*/
+agregarCuenta=function(cuenta){
+    //Si ya existe mostrar un alert CUENTA EXISTENTE
+    //Si se agrega, mostrar un alert CUENTA AGREGADA
+    cuentaExiste = buscarCuenta(cuenta.numeroCuenta);
+    if(cuentaExiste!=null){
+        alert("CUENTA EXISTENTE");
+    }else{
+        cuentas.push(cuenta);
+        alert("CUENTA AGREGADA");
+    }
+}
+
+agregar=function(){
+    //Toma los valores de las cajas de texto, sin validaciones
+    //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
+    //Invoca a agregarCuenta
+    //Invoca a mostrarCuentas
+    let cedula = recuperarTexto("txtCedula");
+    let numeroCuenta = recuperarTexto("txtCuenta");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let saldo = 0;
+    let cuenta = [];
+    cuenta.numeroCuenta = numeroCuenta;
+    cuenta.cedula = cedula;
+    cuenta.nombre = nombre;
+    cuenta.apellido = apellido;
+    cuenta.saldo = saldo;
+    agregarCuenta(cuenta);
+    mostrarCuentas();
+}
+//--------------------------------------------------
+
+// Transacciones.js
+
+//--------------------------------------------------
+
+/*
+    Busca la cuenta en el arreglo en función del número de cuenta,
+    si existe retorna el objeto cuenta, caso contrario retorna null. 
+*/
+/*buscarCuenta=function(numeroCuenta){
+    let cuentaEncontrada = null;
+    for (let i = 0; i < cuentas.length; i++) {
+        if (cuentas[i].numeroCuenta === numeroCuenta) {
+            cuentaEncontrada = cuentas[i];
+            break;
+        }
+    }
+    return cuentaEncontrada;
+}*/
+
+ejecutarBusqueda=function(){
+    //toma el numero de cuenta de la caja de texto
+    //invoca a buscarCuenta y guarda el resultado en una variable
+    //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
+    numCuenta = recuperarTexto("txtCuenta");
+    let cuenta = buscarCuenta(numCuenta);
+    if(cuenta == null){
+        alert("No existe la cuenta");
+    }else{
+        mostrarTexto("Datos","Numero de Cuenta: " + cuenta.numeroCuenta + "\n" +
+        "Cedula: " + cuenta.cedula + "\n" +
+        "Nombre: " + cuenta.nombre + "\n" +
+        "Apellido: " + cuenta.apellido + "\n" +
+        "Saldo: " + cuenta.saldo); 
+        mostrarComponente("txtCantidad");
+        habilitarComponente("btnDepositar");
+        habilitarComponente("btnRetirar");
+    }
+}
+
+
+
+ejecutarDeposito=function(){
+    //Toma el numero de cuenta ingresado en la caja de texto
+    //Toma el monto ingresado en la caja de texto
+    //invoca a depositar
+    //Muestra un mensaje TRANSACCION EXITOSA
+    //Muestra en pantalla el nuevo saldo de la cuenta
+    numCuenta = recuperarTexto("txtCuenta");
+    monto = recuperarFloat("txtCantidad");
+    let cuenta = buscarCuenta(numCuenta);
+    depositar(numCuenta,monto);
+    alert("TRANSACCION EXITOSA");
+    mostrarTexto("Datos","Numero de Cuenta: " + cuenta.numeroCuenta + "\n" +
+        "Cedula: " + cuenta.cedula + "\n" +
+        "Nombre: " + cuenta.nombre + "\n" +
+        "Apellido: " + cuenta.apellido + "\n" +
+        "Saldo: " + cuenta.saldo); 
+        
+}
+
+ejecutarRetiro=function(){    
+    numCuenta = recuperarTexto("txtCuenta");
+    monto = recuperarFloat("txtCantidad");    
+    retirar(numCuenta,monto);
+}
+
+depositar=function(numeroCuenta,monto){
+    let cuentaAfectada;
+    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+    cuentaAfectada = buscarCuenta(numeroCuenta);    
+    cuentaAfectada.saldo += monto;
+}
+
+retirar=function(numeroCuenta,monto){
+    let cuentaAfectada;
+    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    //Valida si la cuenta tiene el saldo suficiente para retirar el monto
+    //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
+    //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
+    //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+    cuentaAfectada = buscarCuenta(numeroCuenta);
+    if(cuentaAfectada.saldo >= monto){
+        cuentaAfectada.saldo -= monto;
+        alert("TRANSACCION EXITOSA");
+        mostrarTexto("Datos","Numero de Cuenta: " + cuentaAfectada.numeroCuenta + "\n" +
+        "Cedula: " + cuentaAfectada.cedula + "\n" +
+        "Nombre: " + cuentaAfectada.nombre + "\n" +
+        "Apellido: " + cuentaAfectada.apellido + "\n" +
+        "Saldo: " + cuentaAfectada.saldo); 
+
+    }else{
+        alert("SALDO INSUFICIENTE");
+    }
+}
+
+//-----------------------------------
+
+// Movimientos.js
+
+//-----------------------------------
+
+ejecutarFiltro=function(){
+    let elemento=recuperarTexto("txtMovimiento");
+    filtrarMovimientos(elemento);
+}
+
+filtrarMovimientos=function(numeroCuenta){
+    let movimientosCuenta=[];
+    //Se barre el arreglo de movimientos
+    for(let i=0;i<movimientos.length;i++){
+    //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
+    //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
+    //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
+        if(numeroCuenta==movimientos[i].numeroCuenta){
+            movimientosCuenta.push(movimientos[i]);
+        }
+    }
+    mostrarMovimientos(movimientosCuenta);
+}
+
+/*
+    Recibe un arreglo con los movimientos que va a mostrar en pantalla
+*/
+mostrarMovimientos=function(misMovimientos){
+    //Muestra en pantalla una tabla con los movimientos que recibe en misMovimientos
+    //Columnas: NUMERO CUENTA, MONTO, TIPO
+    let elemento=document.getElementById("tablaMovimientos");
+    let estructuraTabla="<table><tr>"+
+    "<th>NUMERO CUENTA</th>"+
+    "<th>MONTO</th>"+
+    "<th>TIPO</th></tr>";
+    //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
+    //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
+    //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
+    for(let i=0;i<misMovimientos.length;i++){
+        movimiento=misMovimientos[i];
+        estructuraTabla+="<tr><td>"+movimiento.numeroCuenta+"</td>";
+        if(movimiento.tipo=="D"){
+         estructuraTabla+="<td>"+((movimiento.monto)*-1)+"</td>";
+    }else{
+         estructuraTabla+="<td>"+movimiento.monto+"</td>";
+    }
+    estructuraTabla+= "<td>"+movimiento.tipo+"</td></tr>";
+    }
+    estructuraTabla+="</table>";
+    elemento.innerHTML=estructuraTabla;
+    
+}
